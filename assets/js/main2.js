a983a5a92af228bbcff00f882750f431d5a92016
kaique <kaiquemathor@gmail.com>
2022-12-13 07:56:39 -0300

    add pokedex

---
+++ assets/js/main2.js
@@ -0,0 +1,39 @@
+
+const offset = 0 
+const limit = 10 
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+function convertPokemonToLi(pokemon) {
+	return `
+		<li class="pokemon">
+				<span class="number">#001</span>
+				<span class="name">${pokemon.name}</span>  
+				
+				<div class="detail"> 
+					<ol class="types">
+						<li class="type">grass</li>
+						<li class="type">poison</li>
+					
+					</ol>
+				
+					<img src="\imgs\pokemon\bulbasaur.png" 
+						alt="${pokemon.name}"> 
+				</div>
+			</li>
+		`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+pokeApi.getPokemons().then((pokemons = []) => {		
+
+
+    const newList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon)
+    })
+
+    const newHtml = newList.join()
+
+     pokemonList.innerHTML += newHtml
+	
